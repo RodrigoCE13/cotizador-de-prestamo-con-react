3940c596c248537aeaab4fe5bfb7b203bf5ea27b
Rodrigo Carrasco <rodrigo.carrasco@virginiogomez.cl>
2023-03-28 19:39:04 -0300

    subo cotizador del curso de react

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"//que todos los archivos de src busque los q tengan esas extensiones
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
